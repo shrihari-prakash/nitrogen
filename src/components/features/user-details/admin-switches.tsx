f3df24d78d203b352d2e36419ee9ec45a2b939ac
Shrihari Prakash <shrihariprakash@outlook.com>
2023-10-29 16:50:06 +0530

    Integrated admin switches.

--- src/components/features/user-details/admin-switches.tsx
+++ src/components/features/user-details/admin-switches.tsx
@@ -1,10 +1,39 @@
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { TypographyH4 } from "@/components/ui/typography";
+import axiosInstance from "@/service/axios";
 import { User } from "@/types/user";
+import { useState } from "react";
 
 export default function AdminSwitches({ user }: { user: User }) {
-  console.log(user);
+  const [verified, setVerified] = useState(user.verified);
+  const [suspended, setSuspended] = useState(user.isBanned);
+  const [restricted, setRestricted] = useState(user.isRestricted);
+
+  const onVerifyChange = async (state: boolean) => {
+    await axiosInstance.post("/user/admin-api/verify", {
+      target: user._id,
+      state: state,
+    });
+    return setVerified(state);
+  };
+
+  const onSuspendChange = async (state: boolean) => {
+    await axiosInstance.post("/user/admin-api/ban", {
+      target: user._id,
+      state,
+    });
+    return setSuspended(state);
+  };
+
+  const onRestrictChange = async (state: boolean) => {
+    await axiosInstance.post("/user/admin-api/restrict", {
+      target: user._id,
+      state,
+    });
+    return setRestricted(state);
+  };
+
   return (
     <>
       <TypographyH4>Admin Controls</TypographyH4>
@@ -13,19 +42,19 @@ export default function AdminSwitches({ user }: { user: User }) {
           <div className="space-y-0.5">
             <Label className="text-right">Verified</Label>
           </div>
-          <Switch checked={true} />
+          <Switch checked={verified} onCheckedChange={onVerifyChange} />
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-4">
           <div className="space-y-0.5">
             <Label className="text-right">Suspended</Label>
           </div>
-          <Switch checked={true} />
+          <Switch checked={suspended} onCheckedChange={onSuspendChange} />
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-4">
           <div className="space-y-0.5">
             <Label className="text-right">Restricted</Label>
           </div>
-          <Switch checked={true} />
+          <Switch checked={restricted} onCheckedChange={onRestrictChange} />
         </div>
       </div>
     </>
