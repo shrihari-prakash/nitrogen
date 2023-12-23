45e82ff90e3a2b08d36d878262dc21996bbe4b37
Shri <35889246+shrihari-prakash@users.noreply.github.com>
2023-12-23 13:23:49 +0530

    Update profile-card.tsx

--- src/components/features/user-editor/profile-card.tsx
+++ src/components/features/user-editor/profile-card.tsx
@@ -16,7 +16,7 @@ export default function ProfileCard({ user }: { user: User }) {
   return (
     <Card className='mt-4'>
       <div className='flex flex-col items-start md:flex-row justify-start space-x-4 m-2 md:m-4'>
-        <Avatar className={user.profilePictureUrl ? 'w-28 h-28' : ''}>
+        <Avatar className={user.profilePictureUrl ? 'w-28 h-28 ml-4' : 'ml-4'}>
           <AvatarImage src={user.profilePictureUrl} />
           <AvatarFallback>
             {user.firstName
